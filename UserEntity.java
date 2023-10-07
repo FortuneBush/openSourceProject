8cc67837a0d1301693b15a7071efcb0fcf0a96a9
Wang Yanbo <1368387047@qq.com>
2023-10-07 22:16:55 +0800

    commit userEntity

---
+++ UserEntity.java
@@ -0,0 +1,41 @@
+package com.projectpractice.entity;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @BelongsProject: projectPractice
+ * @BelongsPackage: com.projectpractice.entity
+ * @Author: Tianyu Han
+ * @CreateTime: 2023-06-03  17:18
+ * @Description: User实体
+ * @Version: 1.0
+ */
+@TableName("user_info")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+    @TableId
+    private String id;
+    @ExcelProperty("用户名")
+    private String username;
+    private String password;
+    @ExcelProperty("开始时间")
+    private LocalDateTime startTime;
+    @ExcelProperty("结束时间")
+    private LocalDateTime stopTime;
+    @TableField(fill = FieldFill.INSERT)
+    private String status;
+
+}
